459fcbb78fe934db948b9f082ceaca90ff0ef114
sekhar3 <62094328+sekhar3@users.noreply.github.com>
2020-03-13 19:02:56 +0530

    Update urls.py

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('',include('webapp.urls')),
-    path('admin/', admin.site.urls),
+    	path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
